68696db7d7a432ecc40897ae3511d285ffbfdeee
Nithish <nithish@Nithishs-Air.Dlink>
2022-08-17 06:52:54 +0530

    Hastags

---
+++ pblm7.js
@@ -0,0 +1,15 @@
+function getHashTags(title) {
+    const arr = title.split(" ");
+    const asc = arr.sort((a,b) => b.length - a.length);
+    const hashtag = [];
+    let n = asc.length<=3 ? asc.length:3
+    {
+        for(let i=0;i<3;i++){
+            let c = '#' + asc[i];
+            hashtag.push(c);
+        }
+        console.log(hashtag)
+    }
+}    
+
+getHashTags("How the Avocado Became the Fruit of the Global Trade")
